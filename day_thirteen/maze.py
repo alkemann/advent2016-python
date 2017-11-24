e8cad6fa787d28cff62bdef2045bc9714bd15c85
Alexander Morland <alek.morland@gmail.com>
2017-11-24 17:04:02 +0100

    Part 2 of day 13

--- day_thirteen/maze.py
+++ day_thirteen/maze.py
@@ -1,6 +1,7 @@
 import math
 import sys
 from collections import deque as Queue
+from queue import PriorityQueue
 
 
 def calc(x, y):
@@ -19,7 +20,7 @@ def is_wall(x, y, f):
     return is_binary_wall(binary(calc(x, y) + f))
 
 
-def draw_maze(size, fav, target_x, target_y, path):
+def draw_maze(size, fav, highlight):
     if size >= 10:
         top = "    "
         for i in range(1, size + 1):
@@ -32,8 +33,7 @@ def draw_maze(size, fav, target_x, target_y, path):
     for y in range(0, size+1):
         row = (" " if y < 10 else "") + "%d " % y
         for x in range(0, size+1):
-            # if x == target_x and y == target_y: row += "O"; continue
-            if (x, y) in path: row += "o"; continue
+            if (x, y) in highlight: row += 'o'; continue #str(highlight[(x, y)])
             row += "#" if is_wall(x, y, fav) else "."
         print(row)
 
@@ -45,7 +45,7 @@ class Maze:
 
     def in_bounds(self, id):
         (x, y) = id
-        return x >= 0 and y >= 0
+        return 0 <= x <= 60  and 0 <= y <= 60
 
     def passable(self, id):
         (x, y) = id
@@ -59,39 +59,67 @@ class Maze:
         results = filter(self.passable, results)
         return results
 
-    def map_to(self, start):
+    def map_to(self, start, goal):
         frontier = Queue()
         frontier.append(start)
         came_from = {}
         came_from[start] = None
         while len(frontier) > 0:
             current = frontier.popleft()
+            if current == goal:
+                break
             for to in self.neighbors(current):
                 if to not in came_from:
                     frontier.append(to)
                     came_from[to] = current
         return came_from
 
+    def reachable_in(self, start, steps):
+        frontier = PriorityQueue()
+        frontier.put(start)
+        cost_so_far = {}
+        cost_so_far[start] = 0
+        new_cost = 0
+        while frontier.empty() is False:
+            current = frontier.get()
+            for to in self.neighbors(current):
+                new_cost = cost_so_far[current] + 1
+                if to not in cost_so_far or new_cost < cost_so_far[to]:
+                    cost_so_far[to] = new_cost
+                    frontier.put(to, new_cost)
+        reachable = {}
+        for place in cost_so_far:
+            if cost_so_far[place] <= steps:
+                reachable[place] = cost_so_far[place]
+        return reachable
+
 
 if __name__ == '__main__':
-    size = fav = target_x = target_y = 10
+    size = fav = target_x = target_y = steps = 10
     if len(sys.argv) >= 2: fav = int(sys.argv[1])
     if len(sys.argv) >= 3: target_x  = int(sys.argv[2])
     if len(sys.argv) >= 4: target_y = int(sys.argv[3])
-    if len(sys.argv) >= 5: size= int(sys.argv[4])
-
-    maze = Maze(fav)
-    came_from = maze.map_to((1, 1))
+    if len(sys.argv) >= 5: size = int(sys.argv[4])
+    if len(sys.argv) >= 6: steps = int(sys.argv[5])
 
     target = (target_x, target_y)
-    path = [target]
+    maze = Maze(fav)
+    came_from = maze.map_to((1, 1), target)
+
+    path = {}
+    path[target] = 'o'
     f = came_from[target]
     while f is not None:
-        path.append(f)
+        path[f] = 'o'
         f = came_from[f]
 
+    steps_needed = (len(path) - 1)
+    print("Steps to target: %d" % steps_needed)
+    draw_maze(size, fav, path)
 
-    steps = (len(path) - 1)
-    print("Steps to target: %d" % steps)
-
-    draw_maze(size, fav, target_x, target_y, path)
+    print(" ")
+    print(" ")
+    reachable = maze.reachable_in((1, 1), steps)
+    print(" Reachables in %d steps: %d" % (steps, len(reachable)))
+    print(" ")
+    draw_maze(size, fav, reachable)
